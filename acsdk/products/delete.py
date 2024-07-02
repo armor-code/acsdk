dd4451f85a537817cc7b14beea3efd9bfb5ce7f9
bjenkins-armorcode <153338369+bjenkins-armorcode@users.noreply.github.com>
2024-07-02 13:38:50 +0000

    Updated build artifacts

---
+++ acsdk/products/delete.py
@@ -0,0 +1,10 @@
+import asyncio
+from itertools import chain
+from tqdm.asyncio import tqdm
+
+from util import fetch, Promise
+
+async def delete_product_by_id(session, product_id):
+    response = await fetch(session, "delete", "/user/product/" + str(product_id))
+
+    return response.ok
