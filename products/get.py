081b49f1046d0d7d1ab16d10760eb91396d9dad0
bjenkins-armorcode <153338369+bjenkins-armorcode@users.noreply.github.com>
2024-07-10 14:41:25 +0000

    Updated build artifacts

---
+++ products/get.py
@@ -0,0 +1,49 @@
+import asyncio
+from itertools import chain
+from tqdm.asyncio import tqdm
+
+from ..util import fetch, Promise
+
+def get_products(session, page_number=None):
+    return fetch(session, "get", "/user/product/elastic/paged", params=list({
+        "environmentName": "PRODUCTION",
+        "pageSize": "100",
+        **({"pageNumber": str(page_number)} if page_number is not None else {}),
+        "tags": "",
+        "sortBy": "NAME",
+        "direction": "ASC"
+    }.items()))
+
+async def get_all_products(session):
+    response = await (await get_products(session)).json()
+
+    total_pages = response["totalPages"]
+
+    tasks = []
+
+    if total_pages > 1:
+        for page_number in range(1, total_pages):
+            tasks.append(asyncio.create_task(Promise.reduce_series([
+                get_products(session, page_number),
+                lambda response: response.json(),
+                lambda data: data["content"]
+            ])))
+
+    subproducts = response["content"]
+
+    pages = await tqdm.gather(*tasks)
+
+    subproducts.extend(chain.from_iterable(pages))
+
+    return subproducts
+
+async def get_product_by_id(session, product_id):
+    return await (await fetch(session, "get", "/user/product/" + str(product_id))).json()
+
+async def get_products_by_name(session, product_name):
+    products = list(filter(lambda product: product["name"].casefold() == product_name.casefold(), await get_all_products(session)))
+
+    if len(products) > 1:
+        print("WARN: Multiple matches for product name: " + product_name)
+
+    return products
