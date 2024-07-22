2fefe0e10e9f32249528d04f839480402e02a7f9
bjenkins-armorcode <153338369+bjenkins-armorcode@users.noreply.github.com>
2024-07-22 20:37:22 +0000

    Updated build artifacts

--- acsdk/util/csv.py
+++ acsdk/util/csv.py
@@ -1,26 +1,28 @@
-import csv
-import os
+import csv as csv
 
-def read(file_path):
-    rows = []
+class CSV:
+    @staticmethod
+    def read(file_path):
+        rows = []
 
-    print("Reading " + file_path)
+        print("Reading " + file_path)
 
-    with open(file_path) as file:
-        reader = csv.DictReader(file)
+        with open(file_path) as file:
+            reader = csv.DictReader(file)
 
-        for row in reader:
-            rows.append(row)
+            for row in reader:
+                rows.append(row)
 
-    return rows
+        return rows
 
-def write(file_path, fieldnames, data):
-    rows = list(map(lambda item: dict(list(item.items())), data))
+    @staticmethod
+    def write(file_path, fieldnames, data):
+        rows = list(map(lambda item: dict(list(item.items())), data))
 
-    with open(file_path, "w") as file:
-        writer = csv.DictWriter(file, extrasaction="ignore", fieldnames=fieldnames, quoting=csv.QUOTE_ALL)
+        with open(file_path, "w") as file:
+            writer = csv.DictWriter(file, extrasaction="ignore", fieldnames=fieldnames, quoting=csv.QUOTE_ALL)
 
-        writer.writeheader()
-        writer.writerows(rows)
+            writer.writeheader()
+            writer.writerows(rows)
 
-    print("CSV written to " + file_path)
+        print("CSV written to " + file_path)
